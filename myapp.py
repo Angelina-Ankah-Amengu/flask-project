948f758fa9a27787b69b2061a627bf67cbf8479e
Sam Moorhouse <sam.j.moorhouse@gmail.com>
2017-07-14 10:53:11 +0000

    static css

--- myapp.py
+++ myapp.py
@@ -6,6 +6,11 @@ app = Flask(__name__)
 def index():
 	return render_template("index.html")
 
+
+@app.route('/css/<path:path>')
+def send_js(path):
+    return send_from_directory('css', path)
+
 @app.route('/whereami')
 def whereami():
 	return "Kdua"
