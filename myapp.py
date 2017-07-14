20f727af375d0df71115339a5fca8751331ba383
Sam Moorhouse <sam.j.moorhouse@gmail.com>
2017-07-14 11:07:27 +0000

    static img

--- myapp.py
+++ myapp.py
@@ -8,8 +8,12 @@ def index():
 
 
 @app.route('/css/<path:path>')
-def send_js(path):
+def send_css(path):
     return send_from_directory('css', path)
+
+@app.route('/img/<path:path>')
+def send_img(path):
+    return send_from_directory('img', path)
 
 @app.route('/whereami')
 def whereami():
